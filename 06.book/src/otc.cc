d3a5e5d871d4045ef805995dd3ed640743598f37
liruigang <yueyemingming@163.com>
2021-04-21 07:33:15 +0800

    创建整个目录结构

---
+++ 06.book/src/otc.cc
@@ -0,0 +1,60 @@
+#include <inttypes.h>
+#include "xchain/xchain.h"
+#include "xchain/account.h"
+#include "xchain/contract.pb.h"
+#include "xchain/syscall.h"
+#include "otc.pb.h"
+#include "otc.h"
+
+#include <iostream>
+using namespace std;
+
+Buddha::Buddha() :
+    _guaranty_table(          context(), "guaranty"                 ),
+    _log_table(          context(), "log"                 ),
+
+    ctx(context())
+{
+}
+
+void Buddha::_log_error(const string& file, const string& fun, const int line, const string& message) {
+    cout << file << "(" << line << ") [" << fun << "] " << message << endl;
+    xchain::json ret ;
+    ret["result"] = false;
+    ret["message"] = message;
+
+    ctx->ok(ret.dump());
+}
+
+
+void Buddha::_log_error(const string& file, const string& fun, const int line, const string& message, const xchain::json& j) {
+    cout << file << "(" << line << ") [" << fun << "] " << j.dump() << endl;
+    xchain::json ret ;
+    ret["result"] = false;
+    ret["message"] = message;
+    ret["value"] = j;
+    if(j.is_array())
+        ret["size"] = j.size();
+
+    ctx->ok(ret.dump());
+}
+
+void Buddha::_log_ok(const string& file, const string& fun, const int line, const string& message) {
+    cout << file << "(" << line << ") [" << fun << "] " << message << endl;
+    xchain::json ret ;
+    ret["result"] = true;
+    ret["message"] = message;
+    ctx->ok(ret.dump());
+}
+
+void Buddha::_log_ok(const string& file, const string& fun, const int line, const string& message, const xchain::json& j) {
+    cout << file << "(" << line << ") [" << fun << "] " << j.dump() << endl;
+    xchain::json ret ;
+    ret["result"] = true;
+    ret["message"] = message;
+    ret["value"] = j;
+    if(j.is_array())
+        ret["size"] = j.size();
+
+    ctx->ok(ret.dump());
+}
